17b01775b5d1b1180320313f0114013b3bae419a
Nelocompne <qaqmango@outlook.com>
2022-06-20 04:01:14 +0800

    over

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from requests_html import HTMLSession
 session = HTMLSession()
 
-URL = 'https://steamcommunity.com/groups/cnl4d2rpg'
+URL = 'https://steamcommunity.com/groups/myanimeclan'
 r = session.get(URL+'/memberslistxml/?xml=1')
 g = r.html.find('groupID64', first=True)
 print(int(g.text)-103582791429521408)
-# 实现原因来自 https://forums.alliedmods.net/showthread.php?t=295678
+# 计算方法 https://forums.alliedmods.net/showthread.php?t=295678
