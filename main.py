18a4606c575e7e3dba917ca9d2c76a501547ca0c
Nelocompne <qaqmango@outlook.com>
2022-06-20 04:15:58 +0800

    就这样吧(又不是不能用)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from requests_html import HTMLSession
 session = HTMLSession()
 
 URL = 'https://steamcommunity.com/groups/myanimeclan'
+
 r = session.get(URL+'/memberslistxml/?xml=1')
 g = r.html.find('groupID64', first=True)
 print(int(g.text)-103582791429521408)
