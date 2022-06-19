f279494e3691f1b98c10ef789b5a98f891322219
Nelocompne <qaqmango@outlook.com>
2022-06-20 02:28:36 +0800

    g

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ session = HTMLSession()
 
 URL = 'https://steamcommunity.com/groups/cnl4d2rpg'
 r = session.get(URL+'/memberslistxml/?xml=1')
-gid64 = r.html.find('groupID64', first=True)
-print(int(gid64.text)-103582791429521408)
+g = r.html.find('groupID64', first=True)
+print(int(g.text)-103582791429521408)
 # 实现原因来自 https://forums.alliedmods.net/showthread.php?t=295678
