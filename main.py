8efbf64eb639e119f866b21e2756543fc1104bfb
Nelocompne <qaqmango@outlook.com>
2022-06-20 02:30:21 +0800

    接近了？

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from requests_html import HTMLSession
-session = HTMLSession()
+import requests
+from lxml import html
 
 URL = 'https://steamcommunity.com/groups/cnl4d2rpg'
-r = session.get(URL+'/memberslistxml/?xml=1')
-g = r.html.find('groupID64', first=True)
-print(int(g.text)-103582791429521408)
-# 实现原因来自 https://forums.alliedmods.net/showthread.php?t=295678
+r = requests.get(URL+'/memberslistxml/?xml=1')
+tree = html.fromstring(r.content)
+navareas = tree.xpath('//memberList/groupID64')
+print(navareas)
